edb1da8f91d2f37f3f45acb5914b17caf463963a
Ahhhhman <pawat.pooh@gmail.com>
2023-10-22 11:11:42 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< Updated upstream
 import { FormsModule } from '@angular/forms';
 
-=======
->>>>>>> Stashed changes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
