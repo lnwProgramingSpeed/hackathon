f490570f5a4c0459c32c0ea1bfc9278c1efab761
Ahhhhman <pawat.pooh@gmail.com>
2023-10-23 18:01:37 +0700

    Update my-video-page.component.ts

--- src/app/my-video-page/my-video-page.component.ts
+++ src/app/my-video-page/my-video-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-video-page',
   templateUrl: './my-video-page.component.html',
-  styleUrls: ['./my-video-page.component.css']
+  styleUrls: ['./my-video-page.component.css'],
 })
 export class MyVideoPageComponent {
   numbersArray: number[] = [];
@@ -12,12 +12,11 @@ export class MyVideoPageComponent {
     for (let i = 0; i < randomNum; i++) {
       this.numbersArray.push(i);
     }
-    
   }
 
-  addVideo(){
+  addVideo() {
     let videolength = this.numbersArray.length;
     this.numbersArray.push(videolength++);
-    console.log(videolength + " videos in total");
+    console.log(videolength + ' videos in total');
   }
 }
