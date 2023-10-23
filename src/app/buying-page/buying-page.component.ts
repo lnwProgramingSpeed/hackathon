740eb86ec96d1f58ec8b5bb990824415263961f9
Ahhhhman <pawat.pooh@gmail.com>
2023-10-23 18:01:44 +0700

    buying page

--- src/app/buying-page/buying-page.component.ts
+++ src/app/buying-page/buying-page.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-buying-page',
   templateUrl: './buying-page.component.html',
-  styleUrls: ['./buying-page.component.css']
+  styleUrls: ['./buying-page.component.css'],
 })
 export class BuyingPageComponent {
-
+  numbersArray: number[] = [];
+  constructor() {
+    const randomNum = Math.floor(Math.random() * 3) + 6;
+    for (let i = 0; i < randomNum; i++) {
+      this.numbersArray.push(i);
+    }
+  }
 }
